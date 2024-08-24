6ecd16c6b4aac2968b56bb3199d0117be84b1ab3
Andrew <andrewspurlock@rocketmail.com>
2024-08-24 09:19:52 -0500

    Models created, DbContext file created

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,12 @@ public class User
 {
     public int Id { get; set; }
     [Required]
+    public string Uid { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required] 
+    public string LastName { get; set; }
+    [Required]
     public string? UserName { get; set; }
     public string? Password { get; set; }
     public string? Email { get; set; }
